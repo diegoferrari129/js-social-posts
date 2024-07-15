75803513b59555040024e9f9c63d750ac276e6a5
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-15 18:02:59 +0200

    [UPD] removed from the dom and added to javascript the post section

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -62,6 +62,39 @@ posts.forEach((post) => {
     //dustrutturazione di posts
     let { id, author, content, media, likes, created } = post;
 
+    //aggiungo i post al container
+    document.getElementById('container').innerHTML += `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${author.image}" alt="${author.name}">                
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${author.name}</div>
+                    <div class="post-meta__time">${created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${content}</div>
+        <div class="post__image">
+            <img src="${media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
 });
 
 
